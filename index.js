ff5c7acdea95bf5a5886c1c0db3959bb67b010fb
devin williams <williams.devin702@gmail.com>
2020-06-15 14:45:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var obj = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
-}
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object
