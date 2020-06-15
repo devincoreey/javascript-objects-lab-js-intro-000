54f274d57451698e5e6af29bd854efc4147db9dd
devin williams <williams.devin702@gmail.com>
2020-06-15 14:45:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-function updateObjectWithKeyAndValue(pbject, key, value){
-  const newobj = {prop2:'new value'}
-  return Object.assign({prop:1}, {prop2:2})
+
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
